c49b8e86b3123308c35680ed55a716fd5edd36fc
Naphop Khumchawna <96785512+NutNaphop@users.noreply.github.com>
2023-09-23 18:20:15 +0700

    Update UI and System
    Combine System and UI function at System.c

---
+++ Double_Circular_Queue/System.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int isInt(char *str);
+void clear();
+void wait();
+int isInt(char *str) ; 
+void clear() ;
+void wait() ; 
+
+void displayMenu() {
+    printf("\n");
+    printf("******************* Double Circular Queue *******************\n");
+    printf("* [1] Enqueue at Front                                      *\n");
+    printf("* [2] Enqueue at Rear                                       *\n");
+    printf("* [3] Dequeue from Front                                    *\n");
+    printf("* [4] Dequeue from Rear                                     *\n");
+    printf("* [5] Display Queue                                         *\n");
+    printf("* [6] Exit                                                  *\n");
+    printf("*************************************************************\n");
+}
+
+void displayEF() {
+    printf("\n");
+    printf("******************* Double Circular Queue *******************\n");
+    printf("* [1] Enqueue at front                                      *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*************************************************************\n");
+}
+
+void displayER() {
+    printf("\n");
+    printf("******************* Double Circular Queue *******************\n");
+    printf("*                                                           *\n");
+    printf("* [2] Enqueue at Rear                                       *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*************************************************************\n");
+}
+
+void displayDF() {
+    printf("\n");
+    printf("******************* Double Circular Queue *******************\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("* [3] Dequeue from Front                                    *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*************************************************************\n");
+}
+
+void displayDR() {
+    printf("\n");
+    printf("******************* Double Circular Queue *******************\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("* [4] Dequeue from Rear                                     *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*************************************************************\n");
+}
+
+void displayItem() {
+    printf("\n");
+    printf("******************* Double Circular Queue *******************\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("* [5] Display Queue                                         *\n");
+    printf("*                                                           *\n");
+    printf("*************************************************************\n");
+}
+
+void displayEXIT(){
+	printf("\n");
+    printf("******************* Double Circular Queue *******************\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("*                                                           *\n");
+    printf("* [6] Exit                                                  *\n");
+    printf("*************************************************************\n");
+}
+
+int isInt(char *str) {
+    while (*str) {
+        if (!isdigit(*str)) {
+            return 0; 
+        }
+        str++;
+    }
+    return 1; 
+}
+
+void clear() {
+    system("cls");
+}
+
+void wait() {
+    system("pause");
+}
+
--- Double_Circular_Queue/app2.c
+++ Double_Circular_Queue/app2.c
@@ -2,24 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include "DoubleCircularQueue.h"
+#include "DoubleCircularQueue.c"
+#include "System.c"
 
 int isRunning = 1; 
 int choice; 
 int val; 
 char input[20]; 
-
-void displayMenu() {
-    printf("\n");
-    printf("******************* Double Circular Queue *******************\n");
-    printf("* [1] Enqueue at Front                                      *\n");
-    printf("* [2] Enqueue at Rear                                       *\n");
-    printf("* [3] Dequeue from Front                                   *\n");
-    printf("* [4] Dequeue from Rear                                    *\n");
-    printf("* [5] Display Queue                                        *\n");
-    printf("* [6] Exit                                                 *\n");
-    printf("*************************************************************\n");
-}
 
 int main() {
     while (isRunning) {
@@ -27,11 +16,13 @@ int main() {
         displayMenu();
         printf("Enter your choice: ");
         scanf("%s", input);
-        while (getchar() != '\n') ; // Using for get input only one word , with out whitespace        
+        while (getchar() != '\n') ; // Using for get input only one word , with out whitespace  
+		clear() ;       
         if (isInt(input)) {
             choice = atoi(input); 
             switch (choice) {
                 case 1:
+                	displayEF() ; 
                     printf("Enter the number you want to enqueue at the front: ");
                     scanf("%s", input); 
                     if (isInt(input)) {
@@ -43,6 +34,7 @@ int main() {
                     }
                     break;
                 case 2: 
+                	displayER() ;
                     printf("Enter the number you want to enqueue at the rear: ");
                     scanf("%s", input); 
                     if (isInt(input)){
@@ -55,25 +47,28 @@ int main() {
                     break;
                     
                 case 3:
+                	displayDF() ; 
                     deQueueFront();
                     break;
                     
                 case 4: 
+                	displayDR() ;
                     deQueueRear();  
                     break; 
                     
                 case 5:
+                	displayItem() ;
                     showItem();
                     break;      
                 case 6:
+                	displayEXIT() ;
                     isRunning = 0;
                     printf("Exiting the program.\n");
                     break;
                     
                 default:
                     printf("Invalid choice. Please try again.\n");
-                    break;
-                                        
+                    break;                      
             }
 			wait() ; 
 		}
